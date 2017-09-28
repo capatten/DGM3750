b2f63c713b9e0699833ba30affa25b1cc147287d
capatten <pattenchada@gmail.com>
2017-09-28 16:53:57 -0600

    Set up index.php

--- index.php
+++ index.php
@@ -5,8 +5,41 @@
  * Date: 9/28/2017
  * Time: 4:37 PM
  */
-/>
+?>
 
-<div>
-    This is a branch test
-</div>
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<title>HTML 5 Template</title>
+	</head>
+	<body>
+		<!-- FONTS -->
+		<!--<link rel="stylesheet" href="http://code.jquery.com/mobile/1.4.5/jquery.mobile-1.4.5.min.css" />
+		<link rel="stylesheet" href="<?php /*echo base_url("assets/css/font-awesome.min.css"); */?>" />-->
+
+		<!-- JQUERY -->
+        <!-- Latest compiled and minified CSS -->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+	 	<!--<script src="http://code.jquery.com/jquery-1.11.1.min.js"></script>
+		<script src="http://code.jquery.com/mobile/1.4.5/jquery.mobile-1.4.5.min.js"></script>-->
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+
+        <!-- Material Design Theming -->
+        <link rel="stylesheet" href="https://code.getmdl.io/1.1.3/material.orange-indigo.min.css">
+        <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+
+        <!-- Google Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Roboto:200,300" rel="stylesheet">
+
+		<!--- CUSTOM CSS --->
+		<!--link rel="stylesheet" href="<?php /*echo base_url("assets/css/views/layout/default_desktop.css"); */?>" />-->
+
+        <div class="index-container">
+            <span>Index Page</span>
+        </div>
+
+        <!--script type="text/javascript" src="<?php /*echo base_url("assets/js/views/layout/default_mobile.js"); */?>"></script>-->
+    </body>
+</html>
