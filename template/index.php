91ea40bf8c4f0623037dfe91af17845e7e1c1a32
capatten <pattenchada@gmail.com>
2017-10-01 15:32:47 -0600

    Added JS files

--- template/index.php
+++ template/index.php
@@ -5,25 +5,26 @@
 		<meta charset="utf-8" />
 		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
 		<!--[if lte IE 8]>
-        <script src="assets/js/ie/html5shiv.js"></script><![endif]-->
-		<link rel="stylesheet" href="assets/css/style.css" />
+            <script src="assets/js/ie/html5shiv.js"></script>
+        <![endif]-->
+		<link rel="stylesheet" href="assets/css/style.min.css" />
 	</head>
 	<body>
-		<!-- Header -->
-			<header id="header">
-				<h1>Eventually</h1>
-				<p>A simple template for telling the world when you'll launch<br />
-				your next big thing. Brought to you by <a href="http://html5up.net">HTML5 UP</a>.</p>
-			</header>
+		<!-- NAVIGATION -->
+			<nav>
+                <div class="top-nav">
+                    Navigation
+                </div>
+            </nav>
 
 		<!-- Signup Form -->
-			<form id="signup-form" method="post" action="#">
+			<!--<form id="signup-form" method="post" action="#">
 				<input type="email" name="email" id="email" placeholder="Email Address" />
 				<input type="submit" value="Sign Up" />
-			</form>
+			</form>-->
 
 		<!-- Footer -->
-			<footer id="footer">
+			<!--<footer id="footer">
 				<ul class="icons">
 					<li><a href="#" class="icon fa-twitter"><span class="label">Twitter</span></a></li>
 					<li><a href="#" class="icon fa-instagram"><span class="label">Instagram</span></a></li>
@@ -33,11 +34,12 @@
 				<ul class="copyright">
 					<li>&copy; 2017 Chad Patten</li>
                 </ul>
-			</footer>
+			</footer>-->
 
 		<!-- Scripts -->
 			<!--[if lte IE 8]>
-        <script src="assets/js/ie/respond.min.js"></script><![endif]-->
+                <script src="assets/js/ie/respond.min.js"></script>
+            <![endif]-->
 			<script src="assets/js/main.js"></script>
 
 	</body>
